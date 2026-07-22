837f1de3f03baa12160f711b916bc86e0cf2830b
Hiago 7k <hiago7k@gmail.com>
2026-07-22 18:46:01 -0300

    herdando a classe

---
+++ src/Cachorro.java
@@ -0,0 +1,10 @@
+public class Cachorro extends Animal{
+    
+    public Cachorro(String nome, String raca) {
+        super(nome, raca);
+    }
+
+    public void Latir(){
+        System.out.println("Au au au au");
+    }
+}
