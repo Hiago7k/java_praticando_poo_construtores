4a19c2384b9980e97dd5a8123193a85ae7df40bf
Hiago 7k <hiago7k@gmail.com>
2026-07-22 18:43:59 -0300

    foreach percorrendo lista

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,30 @@
 void main() {
 
-    Produto produto = new Produto("Nome", 0.0, 0);
-    Produto produto1 = new Produto("Teclado Gamer", 199.0, 15);
-    produto.AdicionarProduto(produto1);
+//    Produto produto = new Produto("Nome", 0.0, 0);
+//    Produto produto1 = new Produto("Teclado Gamer", 199.0, 15);
+//    produto.AdicionarProduto(produto1);
+//
+//    Produto produto2 = new Produto("Mouse leve", 500.0, 150);
+//    produto.AdicionarProduto(produto2);
+//
+//    ProdutoPerecivel comida = new ProdutoPerecivel("Pizza", 50.0, 1, "23/07/2026");
+//    produto.AdicionarProduto(comida);
+//
+//    produto.informeLista();
+//    System.out.println(produto1.toString());
+//    System.out.println(produto2.toString());
+//    System.out.println(comida.toString());
 
-    Produto produto2 = new Produto("Mouse leve", 500.0, 150);
-    produto.AdicionarProduto(produto2);
+    ArrayList<String> lista = new ArrayList<>();
+    lista.add("Item 1");
+    lista.add("Item 2");
+    lista.add("Item 3");
+    lista.add("Item 4");
+    lista.add("Item 5");
 
-    ProdutoPerecivel comida = new ProdutoPerecivel("Pizza", 50.0, 1, "23/07/2026");
-    produto.AdicionarProduto(comida);
+    for (var items : lista){
+        System.out.println(items);
+    }
 
-    produto.informeLista();
-    System.out.println(produto1.toString());
-    System.out.println(produto2.toString());
-    System.out.println(comida.toString());
 
 }
