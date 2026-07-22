ff89e009d3d50f1602c86b6dcd642f53ca6321ab
Hiago 7k <hiago7k@gmail.com>
2026-07-22 16:58:17 -0300

    fazendo estancia

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 void main() {
 
+    Produto produto = new Produto();
     Produto produto1 = new Produto();
+    produto1.setNome("Teclado Gamer");
+    produto1.setPreco(199.0);
+    produto1.setQuantidade(15);
+
+    produto.AdicionarProduto(produto1);
+
+    Produto produto2 = new Produto();
+    produto2.setNome("Mouse leve");
+    produto2.setPreco(500.0);
+    produto2.setQuantidade(150);
+    produto.AdicionarProduto(produto2);
 
 
+    produto.informeLista();
 
 }
--- src/Produto.java
+++ src/Produto.java
@@ -8,7 +8,36 @@ public class Produto {
     ArrayList<Produto> listaDeProdutos = new ArrayList<>();
 
     public void AdicionarProduto(Produto produto){
-        produto.add(listaDeProdutos);
+        listaDeProdutos.add(produto);
     }
-    
+
+    public void informeLista(){
+        System.out.println("Tamanho da lista: "+ listaDeProdutos.size());
+        System.out.println(" 01 da lista : "+ listaDeProdutos.get(0).getNome());
+        System.out.println(" 01 da lista : "+ listaDeProdutos.get(1).getNome());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 }
