5556ea9e710bdbd1e0d08667e7e9250471cf8cfb
Hiago 7k <hiago7k@gmail.com>
2026-07-22 18:45:07 -0300

    superclasse animal

---
+++ src/Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    private String nome;
+    private String raca;
+
+    public Animal(String nome, String raca) {
+        this.nome = nome;
+        this.raca = raca;
+    }
+}
