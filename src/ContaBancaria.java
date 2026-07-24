950ccc34aed828d8bcf81fe760463d25158297fd
Hiago 7k <hiago7k@gmail.com>
2026-07-23 22:13:04 -0300

    metodo de adicionar na lista

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContaBancaria {
     private int numeroDaConta;
     private double saldo;
@@ -5,5 +8,11 @@ public class ContaBancaria {
     public ContaBancaria(int numeroDaConta, double saldo) {
         this.numeroDaConta = numeroDaConta;
         this.saldo = saldo;
+    }
+
+    ArrayList<ContaBancaria> lista = new ArrayList<>();
+    
+    public void adicionarNaLista(ContaBancaria conta){
+        lista.add(conta);
     }
 }
